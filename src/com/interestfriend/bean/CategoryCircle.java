b759e9733695aaaa543184dc94d369e3c7bc6ccc
songbinbin <774663576@qq.com>
2014-10-08 21:17:41 +0800

    category circles

---
+++ src/com/interestfriend/bean/CategoryCircle.java
@@ -0,0 +1,31 @@
+package com.interestfriend.bean;
+
+public class CategoryCircle {
+	private String logo = "";
+	private String name = "";
+	private int code;
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+}
