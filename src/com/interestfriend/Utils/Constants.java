417e0b1de7e1899a69fc45c3ea4ca6b7af19ddab
binbin <774663576@qq.com>
2015-03-10 16:55:00 +0800

    升级

--- src/com/interestfriend/Utils/Constants.java
+++ src/com/interestfriend/Utils/Constants.java
@@ -1,11 +1,11 @@
 package com.interestfriend.Utils;
 
 public class Constants {
-	public static final String APP_VSERSION = "1.08";
-	public static final String APP_VSERSION_NAME = "1.0.8";
-	public static final int APP_VSERSION_CODE = 8;
-	public static final String VERSION_INFO = "优化分类搜索圈子";
-	public static final String APP_LINK = "http://123.56.46.254:8080/InterestFriend/app/quyou_1.0.8_2015_02_25.apk";
+	public static final String APP_VSERSION = "1.09";
+	public static final String APP_VSERSION_NAME = "1.0.9";
+	public static final int APP_VSERSION_CODE = 9;
+	public static final String VERSION_INFO = "优化按地区分组,人员查看更直观\n增加添加好友功能\n增加视频通话和语音电话功能";
+	public static final String APP_LINK = "http://123.56.46.254:8080/InterestFriend/app/quyou_1.0.9_2015_03_10.apk";
 	public static final String USER_AGREEMENT = "http://123.56.46.254:8080/InterestFriend/app/user_agreement.html";
 	public static final int NEAR_RAIDUS = 5000;// 附近人距离(米)
 	public static final String APP_AVATAR = "http://123.56.46.254:8080/InterestFriend/images/app_icon.png";
